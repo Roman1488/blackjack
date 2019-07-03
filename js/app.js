b4ca616b5d4b3718c693a268e73f17871a399ee0
Roman Zvyazok <roman.zvyazok@eleks.com>
2019-07-03 15:05:23 +0300

    finish game func

--- js/app.js
+++ js/app.js
@@ -7,7 +7,8 @@ const $dice = document.querySelector('.dice');
 let gameBoard = {
     scores: [0,0],
     roundScore: 0,
-    activePlayer: 0
+    activePlayer: 0,
+    gamePlaying: true
 };
 
 $btnRoll.addEventListener('click', rollBtnClick, false);
@@ -17,26 +18,39 @@ $btnNew.addEventListener('click', init, false);
 init();
 
 function rollBtnClick() {
-    const dice = Math.floor(Math.random() * 6 ) + 1;
-    $dice.style.display = 'block';
-    $dice.src = 'img/dice-' + dice + '.png';
-    if(dice !== 1 ) {
-        gameBoard.roundScore += dice;
-        document.querySelector('#current-' + gameBoard.activePlayer).textContent = gameBoard.roundScore;
-    } else {
-        nextPlayer();
+    if(gameBoard.gamePlaying) {
+        const dice = Math.floor(Math.random() * 6 ) + 1;
+        $dice.style.display = 'block';
+        $dice.src = 'img/dice-' + dice + '.png';
+        if(dice !== 1 ) {
+            gameBoard.roundScore += dice;
+            document.querySelector('#current-' + gameBoard.activePlayer).textContent = gameBoard.roundScore;
+        } else {
+            document.querySelector('.player-' + gameBoard.activePlayer + '-panel').classList.add('flashRed');
+            setTimeout(function (activePlayer) {
+                document.querySelector('.player-' + activePlayer + '-panel').classList.remove('flashRed');
+            }, 1000, gameBoard.activePlayer);
+            nextPlayer();
+        }
     }
 }
 
 function holdBtnClick() {
-    gameBoard.scores[gameBoard.activePlayer] += gameBoard.roundScore;
-    document.querySelector('#score-' + gameBoard.activePlayer).textContent = gameBoard.scores[gameBoard.activePlayer];
-    if(gameBoard.scores[gameBoard.activePlayer] >= 100 ) {
-        document.getElementById('name-' + gameBoard.activePlayer).textContent = 'Winner';
-        $dice.style.display = 'none';
-        document.querySelector('.player-' + gameBoard.activePlayer + '-panel').classList.add('winner');
-    } else {
-        nextPlayer();
+    if(gameBoard.gamePlaying) {
+        gameBoard.scores[gameBoard.activePlayer] += gameBoard.roundScore;
+        document.querySelector('#score-' + gameBoard.activePlayer).textContent = gameBoard.scores[gameBoard.activePlayer];
+        if(gameBoard.scores[gameBoard.activePlayer] >= 20 ) {
+            document.getElementById('name-' + gameBoard.activePlayer).textContent = 'Winner';
+            $dice.style.display = 'none';
+            document.querySelector('.player-' + gameBoard.activePlayer + '-panel').classList.add('winner');
+            gameBoard.gamePlaying = false;
+        } else {
+            document.querySelector('.player-' + gameBoard.activePlayer + '-panel').classList.add('greenFlash');
+            setTimeout(function (activePlayer) {
+                document.querySelector('.player-' + activePlayer + '-panel').classList.remove('greenFlash');
+            }, 1000, gameBoard.activePlayer);
+            nextPlayer();
+        }
     }
 }
 
@@ -58,11 +72,21 @@ function init() {
     gameBoard = {
         scores: [0,0],
         roundScore: 0,
-        activePlayer: 0
+        activePlayer: 0,
+        gamePlaying: true
     };
     [].forEach.call(document.querySelectorAll('.player-current-score'), el => {
         el.textContent = '0';
     });
-    document.querySelector('.player-0-panel').classList.remove('winner');
-    document.querySelector('.player-1-panel').classList.remove('winner');
+    [].forEach.call(document.querySelectorAll('.player-score'), el => {
+        el.textContent = '0';
+    });
+    [].forEach.call(document.querySelectorAll('.player-panel'), el => {
+        el.classList.remove('winner');
+    });
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    $dice.style.display = 'none';
 }
